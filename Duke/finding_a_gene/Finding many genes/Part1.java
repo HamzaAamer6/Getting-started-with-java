d08d2a992f371a5f2583964a8f6f1688d4a2af3e
Hamza Aamer <88921596+HamzaAamer6@users.noreply.github.com>
2023-02-18 02:23:52 +0500

    update

--- Duke/finding_a_gene/Finding many genes/Part1.java
+++ Duke/finding_a_gene/Finding many genes/Part1.java
@@ -15,7 +15,7 @@ public class Part1 {
             if ((end-startIndex)%3==0){
                 return end;
             }
-            System.out.print("\""+end+"\"");
+            //System.out.print("\""+(end-startIndex)+"\"");
             temp=end+stopCodon.length();
         }
         return -1;
@@ -41,26 +41,56 @@ public class Part1 {
         int taa=findStopCodon(dna,start,"TAA");
         int tag=findStopCodon(dna,start,"TAG");
         int tga=findStopCodon(dna,start,"TGA");
+        //System.out.print("\n TAG : "+tag+"\n TAA : "+taa+"\n TGA : "+tga+"\n");
         if((taa ==-1) && (tag==01) && (tga==01)) return "";
         int max=-1;
         if (taa>tag && taa>tga) max=taa;
-        if (tag>taa && tag>tga) max=taa;
-        if (tga>taa && tga>tag) max=taa;
+        if (tag>taa && tag>tga) max=tag;
+        if (tga>taa && tga>tag) max=tga;
         
         return dna.substring(start,max+3);
     }
     void testFindGene(){
-        String test1="------ATG---_TAA--TAA---";//should print ATG---_TAA--TAA
-        String test2="---TAA___TAA---TAA";//should print nothing
-        String test3="ATG123456789-----TAA----TAA"; //should printeverything
-        String test4="12ATG3TAA--TAA";//shouldprint ATG3TAA--TAA
-        String test5="12ATGTGA--TAA";//shouldprint ATGTGA
+        String test1="AATGCTAACTAGCTGACTAAT";//should print ATG---_TAA--TAA
+
         
         System.out.println("Getting Results!\n");
-        System.out.println(" 1 :"+findStopCodon(test1,0,"TAA"));
-        System.out.println(" 2 :"+findStopCodon(test2,0,"TAA"));
-        System.out.println(" 3 :"+findStopCodon(test3,9,"TAA"));
-        System.out.println(" 4 :"+findStopCodon(test4,3,"TAA"));
+        System.out.println(" 1 :"+findGene(test1));
         
     }
+    void printAllGenes(String dna){
+        int start=0;
+        int end=dna.length();
+        int counter=0;
+        while(findGene(dna.substring(start,end))!=""){
+            System.out.println(findGene(dna.substring(start,end)));
+            start=dna.indexOf(findGene(dna.substring(start,end)))+(findGene(dna.substring(start,end))).length();
+            counter++;
+        }
+        System.out.print("There are " + counter + " genes present ! \n");
+    }
+    public void main(){
+        String dna="------ATG---_TAA--TAA------TAA___TAA---TAAATG123456789-----TAA----TAA12ATG3TAA--TGA123ATGTGA--TAA";
+        printAllGenes(dna);
+    }
+    public void findAbc(String input) {
+    int index = input.indexOf("abc");
+    while (true) {
+        if (index == -1) {
+            break;
+        }
+        System.out.print((index+1)+" "+(index+4));
+        System.out.println("a"+index+"a");
+        String found = input.substring(index+1, index+4);
+        System.out.println(found);
+
+        System.out.println("index " + index);
+        index = input.indexOf("abc", index+3);
+        System.out.println("index after updating " + index);
+    }
+}
+public void test() {
+    //findAbc("abcd");
+    findAbc("abcbabccabcd");
+}
 }
